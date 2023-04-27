dd3ba6de0b25ff5c6212ae477fa7fad29c1da41b
Richard_Madson <126484299+RichardMadson@users.noreply.github.com>
2023-04-27 16:14:23 -0300

    Meu-Portfolio

---
+++ index.js
@@ -0,0 +1,31 @@
+var imagem1 = document.getElementById('idade');
+function redirecionarLink1(link){
+    window.open(link);
+  }
+imagem1.addEventListener("mouseover", function() {
+  imagem1.style.cursor = "pointer";
+});
+
+var imagem2 = document.getElementById('hora')
+function redirecionarLink2(link){
+    window.open(link);
+}
+imagem2.addEventListener("mouseover", function() {
+    imagem2.style.cursor = "pointer";
+  });
+
+var imagem3 = document.getElementById('contar');
+function redirecionarLink3(link){
+    window.open(link);
+  }
+imagem3.addEventListener("mouseover", function() {
+  imagem3.style.cursor = "pointer";
+});
+
+var imagem4 = document.getElementById('tabu');
+function redirecionarLink4(link){
+    window.open(link);
+  }
+imagem4.addEventListener("mouseover", function() {
+  imagem4.style.cursor = "pointer";
+});
